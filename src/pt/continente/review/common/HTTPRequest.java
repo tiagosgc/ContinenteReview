50f953af51e578a309f989d35c3cb06fdad72ad4
tacarvalho <tacarvalho@PTMTD005NB0055.mch.moc.sgps>
2012-10-31 13:10:33 +0000

    Resolvido o erro quando le EANs desconhecidos
    Passei a disparar eu uma excepção própria do package, quando o documento
    de retorno tem como primeiro filho null ou != article (excepções
    diferentes).
    
    NÃO FIZ NADA QUANTO ÀS DIMENSÕES!

--- src/pt/continente/review/common/HTTPRequest.java
+++ src/pt/continente/review/common/HTTPRequest.java
@@ -14,6 +14,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,17 +24,17 @@ import android.os.Message;
 
 public class HTTPRequest extends Thread {
 	private static final String TAG = "CntRev - HTTPRequest";
-	
+
 	private String urlBeingSought;
 	private HttpResponse response;
 	private Handler parentHandler;
 	private int requestType;
-	
+
 	public static class requestTypes {
 		public static final int GET_ARTICLE = 1;
 		public static final int GET_DIMENSIONS = 2;
 	}
-	
+
 	public static class responseOutputs {
 		public final static int SUCCESS = 10;
 		public final static int FAILED_ERROR_ON_SUPPLIED_URL = 11;
@@ -51,17 +54,17 @@ public class HTTPRequest extends Thread {
 	public void run() {
 		super.run();
 		Common.log(5, TAG, "run: started");
-
+		String firstChild = null;
 		// simulates delay in fetch
 		boolean simulateDelay = true;
 		try {
 			Thread.sleep(simulateDelay ? 2000 : 1);
-        } catch (InterruptedException e) {
-			Common.log(1, TAG, "run: ERROR in applying delay - " + e.getMessage());
+		} catch (InterruptedException e) {
+			Common.log(1, TAG,
+					"run: ERROR in applying delay - " + e.getMessage());
 			e.printStackTrace();
-        }
+		}
 		Common.log(5, TAG, "run: terminou o delay forçado");
-
 		Message messageToParent = null;
 		DefaultHttpClient client = null;
 		HttpContext localContext = null;
@@ -73,11 +76,11 @@ public class HTTPRequest extends Thread {
 			e.printStackTrace();
 		}
 		Common.log(5, TAG, "run: criou as variáveis chave");
-		
+
 		messageToParent.what = 0;
 		response = null;
 		HttpGet httpGet = null;
-		
+
 		Common.log(5, TAG, "run: vai criar objeto GET");
 		try {
 			httpGet = new HttpGet(urlBeingSought);
@@ -85,23 +88,29 @@ public class HTTPRequest extends Thread {
 			Common.log(1, TAG, "run: ERROR in supplied url - " + e.getMessage());
 			messageToParent.what = responseOutputs.FAILED_ERROR_ON_SUPPLIED_URL;
 		}
-		
+
 		if (messageToParent.what != 0) {
 			parentHandler.sendMessage(messageToParent);
 			return;
 		}
-		
+
 		Common.log(5, TAG, "run: vai obter dados da net");
 		try {
 			response = client.execute(httpGet, localContext);
 		} catch (ClientProtocolException e) {
-			Common.log(1, TAG, "run: ERROR obtaining response to internet query (ClientProtocolException) - " + e.getMessage());
+			Common.log(1, TAG,
+					"run: ERROR obtaining response to internet query (ClientProtocolException) - "
+							+ e.getMessage());
 			messageToParent.what = responseOutputs.FAILED_QUERY_FROM_INTERNET;
 		} catch (IOException e) {
-			Common.log(1, TAG, "run: ERROR obtaining response to internet query (IOException) - " + e.getMessage());
+			Common.log(1, TAG,
+					"run: ERROR obtaining response to internet query (IOException) - "
+							+ e.getMessage());
 			messageToParent.what = responseOutputs.FAILED_QUERY_FROM_INTERNET;
 		} catch (Exception e) {
-			Common.log(1, TAG, "run: ERROR obtaining response to internet query (UndefinedException) - " + e.getMessage());
+			Common.log(1, TAG,
+					"run: ERROR obtaining response to internet query (UndefinedException) - "
+							+ e.getMessage());
 			e.printStackTrace();
 			messageToParent.what = responseOutputs.FAILED_QUERY_FROM_INTERNET;
 		}
@@ -110,8 +119,7 @@ public class HTTPRequest extends Thread {
 			parentHandler.sendMessage(messageToParent);
 			return;
 		}
-		
-		
+
 		Common.log(5, TAG, "run: vai processar respostas");
 		if (response == null) {
 			Common.log(3, TAG, "run: got empty response from query");
@@ -119,46 +127,96 @@ public class HTTPRequest extends Thread {
 			parentHandler.sendMessage(messageToParent);
 			return;
 		} else {
-			Common.log(5, TAG, "run: vai obter o documento a partir da resposta");
+			Common.log(5, TAG,
+					"run: vai obter o documento a partir da resposta");
 			Document newDocument = null;
 			try {
-				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+				DocumentBuilderFactory dbf = DocumentBuilderFactory
+						.newInstance();
+
+				dbf.setValidating(false);
+				// dbf.setNamespaceAware(true);
 				dbf.setIgnoringElementContentWhitespace(true);
 				DocumentBuilder builder = dbf.newDocumentBuilder();
-				
+
+				builder.setErrorHandler(new ErrorHandler() {
+					@Override
+					public void error(SAXParseException arg0)
+							throws SAXException {
+						Common.log(5, TAG, "document builder error 1 ");
+						throw arg0;
+					}
+
+					@Override
+					public void fatalError(SAXParseException arg0)
+							throws SAXException {
+						Common.log(5, TAG, "document builder error 2 ");
+						throw arg0;
+					}
+
+					@Override
+					public void warning(SAXParseException arg0)
+							throws SAXException {
+						Common.log(5, TAG, "document builder error 3 ");
+						throw arg0;
+					}
+				});
 				Common.log(5, TAG, "run: vai processar entity");
 				HttpEntity entity = response.getEntity();
 				Common.log(5, TAG, "run: vai processar stream");
 				InputStream instream = entity.getContent();
 				Common.log(5, TAG, "run: vai processar document");
 				newDocument = builder.parse(instream);
+				newDocument.normalizeDocument();
+				newDocument.normalize();
+				firstChild = newDocument.getChildNodes().item(0).getNodeName();
+				if (firstChild == null) {
+					HttpResponseException e = new HttpResponseException(
+							"Documento retornado é vazio");
+					e.setUrl(urlBeingSought);
+					throw e;
+				}
+
+				Common.log(5, TAG,
+						"run: criar a mensagem a partir do documento");
+				Bundle messageData = new Bundle();
+
+				switch (requestType) {
+				case requestTypes.GET_ARTICLE:
+					Common.log(5, TAG, "run: vai processar artigo");
+					if (firstChild.compareTo("article") != 0) {
+						HttpResponseException e = new HttpResponseException(
+								"Documento não tem como primeiro elemento um artigo");
+						e.setUrl(urlBeingSought);
+						throw e;
+					}
+					Article newArticle = HTTPResponseProcessor
+							.getProductFromDoc(newDocument);
+					messageData.putSerializable("response", newArticle);
+					break;
+				case requestTypes.GET_DIMENSIONS:
+					Common.log(5, TAG, "run: vai processar dimensões");
+					DimensionsList newDimList = HTTPResponseProcessor
+							.getDimensionsFromDoc(newDocument);
+					messageData.putSerializable("response", newDimList);
+					break;
+				}
+
+				Common.log(5, TAG, "run: vai finalizar e enviar mensagem");
+				messageToParent.what = responseOutputs.SUCCESS;
+				messageToParent.setData(messageData);
+				parentHandler.sendMessage(messageToParent);
 			} catch (Exception e) {
-				Common.log(1, TAG, "run: ERROR processing the returned object - " + e.getMessage());
+				Common.log(
+						1,
+						TAG,
+						"run: ERROR processing the returned object - "
+								+ e.getMessage());
 				messageToParent.what = responseOutputs.FAILED_PROCESSING_RETURNED_OBJECT;
 				parentHandler.sendMessage(messageToParent);
 				return;
 			}
-			
-			Common.log(5, TAG, "run: criar a mensagem a partir do documento");
-	        Bundle messageData = new Bundle();
-			
-			switch (requestType) {
-			case requestTypes.GET_ARTICLE:
-				Common.log(5, TAG, "run: vai processar artigo");
-				Article newArticle = HTTPResponseProcessor.getProductFromDoc(newDocument);
-		        messageData.putSerializable("response", newArticle);
-				break;
-			case requestTypes.GET_DIMENSIONS:
-				Common.log(5, TAG, "run: vai processar dimensões");
-				DimensionsList newDimList = HTTPResponseProcessor.getDimensionsFromDoc(newDocument);
-		        messageData.putSerializable("response", newDimList);
-				break;
-			}
-
-			Common.log(5, TAG, "run: vai finalizar e enviar mensagem");
-			messageToParent.what = responseOutputs.SUCCESS;
-	        messageToParent.setData(messageData);
-			parentHandler.sendMessage(messageToParent);
 		}
 		Common.log(5, TAG, "run: finished");
 	}
