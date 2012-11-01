cffd0a2da1775bfe51377faeab4983aadc0ee909
tacarvalho <tacarvalho@PTMTD005NB0055.mch.moc.sgps>
2012-11-01 14:17:27 +0000

    Upstream da Excepção

--- src/pt/continente/review/common/HTTPResponseException.java
+++ src/pt/continente/review/common/HTTPResponseException.java
@@ -2,43 +2,20 @@ package pt.continente.review.common;
 
 public class HTTPResponseException extends Exception {
 
-	String detailMessage ="";
 	String url = "";
+	String msg = "";
+	public HTTPResponseException(String url) {
+		// TODO Auto-generated constructor stub
+		this.msg = url;
+	}
+
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 3703279369859671570L;
-
-	public HTTPResponseException() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public HTTPResponseException(String detailMessage) {
-		super(detailMessage);
-		// TODO Auto-generated constructor stub
-	}
-
-	public HTTPResponseException(Throwable throwable) {
-		super(throwable);
-		// TODO Auto-generated constructor stub
-	}
-
-	public HTTPResponseException(String detailMessage, Throwable throwable) {
-		super(detailMessage, throwable);
-		// TODO Auto-generated constructor stub
-	}
-	public void setUrl(String url) {
-		
-	}
-	public void setDetailMessage(String message) {
-		detailMessage = message;
-	}
-	public String getMessage() {
-		return "URL:"+url+"\n"+detailMessage;
-	}
-	public String toString()	{
-		return this.getMessage();
+	private static final long serialVersionUID = -2141875753471565057L;
+	public void setUrl(String urlBeingSought) {
+		// TODO Auto-generated method stub
+		this.msg = urlBeingSought;
 	}
 
 }
